3896c05ba0cafc53ff9bf4fc8e8a13576742edf4
Dymitr Dragalow <dzmitry.drahaliou@comarch.com>
2022-08-09 15:24:35 +0200

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { HeaderComponent } from './header/header.component';
+import {FlexModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
